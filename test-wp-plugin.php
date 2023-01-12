075bf68e77da6efe9bb4630059000a289ccbe4b4
Sina Rahmannejad <46980823+sinarahmany@users.noreply.github.com>
2023-01-11 19:39:18 -0800

    first commit

---
+++ test-wp-plugin.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Test Wp Plugin
+Plugin URI: https://sinarahmannejad.com/
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: sinarahmannejad
+Author URI: https://sinarahmannejad.com/
+License: A "Slug" license name e.g. GPL2
+*/
